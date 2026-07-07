c24be1b91b0153eb79ba2261b2b6f346a4de66f5
Mériadec Denier d'Aprigny <113612538+Meeria@users.noreply.github.com>
2026-07-07 10:52:47 +0200

    config S3 bucket
    Pour qu'ansible puidse tranferer des fichiers de config

--- UrbanHub/infra/terraform/compute.tf
+++ UrbanHub/infra/terraform/compute.tf
@@ -26,3 +26,40 @@ resource "aws_s3_bucket" "ssm_transfer" {
     Name = "${var.project_name}-ssm-transfer"
   }
 }
+
+resource "aws_iam_role" "ec2_ssm" {
+  name = "${var.project_name}-ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy" "ssm_transfer_bucket" {
+  name = "${var.project_name}-ssm-transfer-bucket-access"
+  role = aws_iam_role.ec2_ssm.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["s3:GetObject", "s3:PutObject"]
+      Resource = "${aws_s3_bucket.ssm_transfer.arn}/*"
+    }]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_ssm" {
+  name = "${var.project_name}-ec2-ssm-profile"
+  role = aws_iam_role.ec2_ssm.name
+}
