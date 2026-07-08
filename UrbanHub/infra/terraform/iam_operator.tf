b07f890a1584fd46dd334853dd765d69354cd855
Mériadec Denier d'Aprigny <113612538+Meeria@users.noreply.github.com>
2026-07-08 14:56:37 +0200

    maj roles

--- UrbanHub/infra/terraform/iam_operator.tf
+++ UrbanHub/infra/terraform/iam_operator.tf
@@ -23,6 +23,7 @@ resource "aws_iam_policy" "operator_least_privilege" {
           "secretsmanager:PutSecretValue",
           "secretsmanager:GetSecretValue",
           "secretsmanager:DescribeSecret",
+          "secretsmanager:GetResourcePolicy",
           "secretsmanager:TagResource"
         ]
         Resource = "arn:aws:secretsmanager:${var.region}:*:secret:${var.project_name}/*"
@@ -32,8 +33,8 @@ resource "aws_iam_policy" "operator_least_privilege" {
         Effect   = "Allow"
         Action   = [
           "iam:CreateRole", "iam:DeleteRole", "iam:GetRole",
-          "iam:PutRolePolicy", "iam:DeleteRolePolicy", "iam:GetRolePolicy",
-          "iam:AttachRolePolicy", "iam:DetachRolePolicy",
+          "iam:PutRolePolicy", "iam:DeleteRolePolicy", "iam:GetRolePolicy", "iam:ListRolePolicies",
+          "iam:AttachRolePolicy", "iam:DetachRolePolicy", "iam:ListAttachedRolePolicies",
           "iam:CreateInstanceProfile", "iam:DeleteInstanceProfile",
           "iam:AddRoleToInstanceProfile", "iam:RemoveRoleFromInstanceProfile",
           "iam:GetInstanceProfile", "iam:TagRole", "iam:PassRole"
@@ -41,16 +42,42 @@ resource "aws_iam_policy" "operator_least_privilege" {
         Resource = "arn:aws:iam::*:role/${var.project_name}-*"
       },
       {
+        Sid      = "IamPolicyForOperatorOnly"
+        Effect   = "Allow"
+        Action   = [
+          "iam:CreatePolicy", "iam:DeletePolicy", "iam:GetPolicy",
+          "iam:GetPolicyVersion", "iam:ListPolicyVersions",
+          "iam:CreatePolicyVersion", "iam:DeletePolicyVersion", "iam:TagPolicy"
+        ]
+        Resource = "arn:aws:iam::*:policy/${var.project_name}-*"
+      },
+      {
+        Sid      = "IamUserForOperatorOnly"
+        Effect   = "Allow"
+        Action   = [
+          "iam:CreateUser", "iam:DeleteUser", "iam:GetUser", "iam:TagUser",
+          "iam:CreateAccessKey", "iam:DeleteAccessKey", "iam:ListAccessKeys", "iam:UpdateAccessKey",
+          "iam:AttachUserPolicy", "iam:DetachUserPolicy", "iam:ListAttachedUserPolicies"
+        ]
+        Resource = "arn:aws:iam::*:user/${var.project_name}-*"
+      },
+      {
         Sid      = "CloudWatchLogsForBackend"
         Effect   = "Allow"
         Action   = [
           "logs:CreateLogGroup",
           "logs:DeleteLogGroup",
-          "logs:DescribeLogGroups",
           "logs:PutRetentionPolicy",
-          "logs:TagResource"
+          "logs:TagResource",
+          "logs:ListTagsForResource"
         ]
         Resource = "arn:aws:logs:${var.region}:*:log-group:/${var.project_name}/*"
+      },
+      {
+        Sid      = "CloudWatchLogsDescribe"
+        Effect   = "Allow"
+        Action   = ["logs:DescribeLogGroups"]
+        Resource = "*"
       },
       {
         Sid      = "SsmSessionForDeployment"
